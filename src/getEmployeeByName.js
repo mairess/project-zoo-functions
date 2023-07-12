f1e84342c6360c8a943cb5be4b59db460c5dc998
Maires Souza <trabalho.maires@gmail.com>
2023-07-12 12:04:18 -0300

    feat: function getEmployeeByName implemented

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
-
+const getEmployeeByName = (employeeName) =>
+  (employeeName === undefined ? {} : data.employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName));
+console.log(getEmployeeByName('Burl'));
 module.exports = getEmployeeByName;
