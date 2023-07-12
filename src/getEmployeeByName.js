0d264a005a1ae8dc47408339b08362a44d32bb8e
Maires Souza <trabalho.maires@gmail.com>
2023-07-12 16:44:22 -0300

    remove: log from function getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,5 +3,4 @@ const data = require('../data/zoo_data');
 const getEmployeeByName = (employeeName) =>
   (employeeName === undefined ? {} : data.employees.find((employee) =>
     employee.firstName === employeeName || employee.lastName === employeeName));
-console.log(getEmployeeByName('Burl'));
 module.exports = getEmployeeByName;
