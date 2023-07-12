3ebb21c1c7a93216d9f5d3dbaa0024ac2e51920b
Maires Souza <trabalho.maires@gmail.com>
2023-07-12 10:20:58 -0300

    refactor: function getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) =>
-  ids.reduce((acc, id) => acc.concat(data.species.filter((specie) => specie.id === id)), []);
+  ids.reduce((acc, id) => [...acc, ...data.species.filter((specie) => specie.id === id)], []);
 // const getSpeciesByIds = (...ids) => ids.flatMap((id) => data.species
 //   .filter((specie) => specie.id === id));
 module.exports = getSpeciesByIds;
