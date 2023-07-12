3cbce6e38d1e6340be42fae623d4218c25eed68f
Maires Souza <trabalho.maires@gmail.com>
2023-07-12 10:39:51 -0300

    feat: function getAnimalsOlderThan implemented

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-};
-
+const getAnimalsOlderThan = (animal, age) =>
+  data.species.filter((animals) => animals.name === animal)
+    .every((residents) => residents.residents[0].age > age);
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
