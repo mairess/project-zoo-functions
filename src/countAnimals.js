91c9ec84ea60c43ec53479452ccde6b3587cd063
Maires Souza <trabalho.maires@gmail.com>
2023-07-13 10:32:58 -0300

    feat: function countAnimals implemented

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (animal.species && animal.sex) {
+    return data.species.find((specie) => specie.name === animal.species).residents
+      .filter((specie) => specie.sex === animal.sex).length;
+  }
+  if (animal.species) {
+    return data.species.find((specie) => specie.name === animal.species).residents.length;
+  }
 };
-
+console.log(countAnimals({ species: 'bears', sex: 'female' }));
 module.exports = countAnimals;
