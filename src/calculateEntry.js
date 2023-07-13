b9f7ff7824a9f6ea15085ce4ef70d9049f23a149
Maires Souza <trabalho.maires@gmail.com>
2023-07-13 20:11:42 -0300

    feat: function calculateEntry implemented

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const { adult: adultPrice, senior: seniorPrice, child: childPrice } = data.prices;
+  const total = adultPrice * adult + seniorPrice * senior + childPrice * child;
+  return total;
 };
 
 module.exports = { calculateEntry, countEntrants };
