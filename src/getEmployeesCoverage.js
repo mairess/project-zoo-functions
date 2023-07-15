78828926577cc1405d8a275931f4811f0c62a6e1
Maires Souza <trabalho.maires@gmail.com>
2023-07-15 12:16:44 -0300

    feat: function getEmployeesCoverage implemented

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getLocation = ([...ids]) => {
+  const locations = [];
+  ids.forEach((id) => {
+    const { location } = data.species.find((specie) => specie.id === id);
+    locations.push(location);
+  });
+
+  return locations;
+};
+
+const getName = ([...ids]) => {
+  const names = [];
+  ids.forEach((id) => {
+    const { name } = data.species.find((specie) => specie.id === id);
+    names.push(name);
+  });
+
+  return names;
+};
+
+const getFullCoverage = () => data.employees.map((employee) => {
+  const { firstName, lastName, id, responsibleFor: species } = employee;
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: getName(species),
+    locations: getLocation(species) };
+});
+
+const getEmployeesCoverage = ({ name, id } = {}) => {
+  if (name || id) {
+    const employeeInformations = data.employees.find((employee) =>
+      employee.firstName === name || employee.lastName === name || employee.id === id);
+    if (employeeInformations) {
+      const { firstName, lastName, id: personId, responsibleFor: species } = employeeInformations;
+      return { id: personId,
+        fullName: `${firstName} ${lastName}`,
+        species: getName(species),
+        locations: getLocation(species) };
+    }
+    throw new Error('Informações inválidas');
+  }
+  return getFullCoverage();
 };
 
 module.exports = getEmployeesCoverage;
